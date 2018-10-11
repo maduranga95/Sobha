def40c6b1b44d39a1fe6f62f3c8cd1b8572b7095
Udhan <udhanisuranga@gmail.com>
2018-10-11 10:48:07 +0530

    LogIn modified, SignUp created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
+
 import { LoginPage } from '../pages/login/login';
+
 @Component({
   templateUrl: 'app.html'
 })

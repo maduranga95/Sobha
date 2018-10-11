3651299e0ff8c7f0c0d3d3d82ffd0e4d309c636d
rAWhiDe <ramitha.16@cse.mrt.ac.lk>
2018-10-11 11:38:32 +0530

    Some spaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
